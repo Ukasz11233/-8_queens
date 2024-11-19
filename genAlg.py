6874629ef81694a1e61e02d647608839a1a02d83
Ukasz11233 <chmiel11233@gmail.com>
2024-11-19 21:57:08 +0100

    Added queens respawns in column

--- genAlg.py
+++ genAlg.py
@@ -9,26 +9,21 @@ def select_parents(pop, fitness, num_parents):
         fitness[max_fitness_idx] = -float("Inf")
     return parents
 
-
 def crossover(parents, offspring_size):
     offspring = numpy.empty(offspring_size)
-    crossover_point = numpy.uint8(offspring_size[1]/2)
+    crossover_point = numpy.uint8(offspring_size[1] / 2)
 
     for k in range(offspring_size[0]):
-        parent1_idx = k%parents.shape[0]
-        parent2_idx = (k+1)%parents.shape[0]
+        parent1_idx = k % parents.shape[0]
+        parent2_idx = (k + 1) % parents.shape[0]
         offspring[k, 0:crossover_point] = parents[parent1_idx, 0:crossover_point]
         offspring[k, crossover_point:] = parents[parent2_idx, crossover_point:]
     return offspring
 
-
 def mutation(offspring_crossover, num_mutations):
-    mutations_counter = numpy.uint8(offspring_crossover.shape[1]/num_mutations)
     for idx in range(offspring_crossover.shape[0]):
-        gene_idx = mutations_counter-1
         for mutation_num in range(num_mutations):
-            random_value = numpy.random.randint(low=0, high=8, size=None)
-            offspring_crossover[idx, gene_idx] = random_value
-            gene_idx = gene_idx + mutations_counter
-
+            gene_idx = numpy.random.randint(low=0, high=8)  # Flip a queen's position
+            new_value = numpy.random.randint(0, 8)
+            offspring_crossover[idx, gene_idx] = new_value
     return offspring_crossover
--- main.py
+++ main.py
@@ -12,15 +12,10 @@ class Queens8App(kivy.app.App):
     pop_created = False
 
     def start_ga(self, *args):
-        """
-        Initializes the genetic algorithm process, iterating through generations to optimize the 
-        population_board. Stops when the best solution is found or the maximum number of generations is reached.
-        Saves the best solutions to files and updates the UI with the best solution.
-        """
         self.initialize_population()
         best_outputs = []
         best_outputs_fitness = []
-        if self.pop_created == False:
+        if not self.pop_created:
             return
 
         num_generations = numpy.uint16(self.num_generations_TextInput.text)
@@ -30,11 +25,9 @@ class Queens8App(kivy.app.App):
             print("\n##  Generation = ", generation, "  ##\n")
 
             population_fitness, total_num_attacks = self.fitness(self.population_board)
-            # print("\nFitness : \n", population_fitness)
 
             max_fitness = numpy.max(population_fitness)
             max_fitness_idx = numpy.where(population_fitness == max_fitness)[0][0]
-            # print("\nMax Fitness = ", max_fitness)
 
             best_outputs_fitness.append(max_fitness)
             best_outputs.append(self.population_1D_vector[max_fitness_idx, :])
@@ -44,9 +37,6 @@ class Queens8App(kivy.app.App):
                 self.num_attacks_Label.text = "Best Solution Found"
                 print("\n1D population_board : \n", self.population_1D_vector)
                 print("\n**  Best solution IDX = ", max_fitness_idx, "  **\n")
-
-                best_outputs_fitness_array = numpy.array(best_outputs_fitness)
-                best_outputs_array = numpy.array(best_outputs)
 
                 numpy.save("best_outputs_fitness.npy", best_outputs_fitness)
                 numpy.save("best_outputs.npy", best_outputs)
@@ -63,34 +53,19 @@ class Queens8App(kivy.app.App):
 
             self.vector_to_matrix()
 
-    # Initialize the population_board for the Genetic Algorithm
     def initialize_population(self, *args):
-        """
-        Initializes the population_board matrix with random queen positions for the 8-queens problem.
-        Converts the vector representation of solutions into matrix form for further processing.
-        """
         self.num_solutions = numpy.uint8(self.num_solutions_TextInput.text)
 
         self.reset_board_text()
 
-        self.population_1D_vector = numpy.zeros(shape=(self.num_solutions, 8))  # Rows represent solutions.
-
-        for solution_idx in range(self.num_solutions):
-            initial_queens_y_indices = numpy.random.rand(8) * 8
-            initial_queens_y_indices = initial_queens_y_indices.astype(numpy.uint8)
-            self.population_1D_vector[solution_idx, :] = initial_queens_y_indices
+        self.population_1D_vector = numpy.random.randint(0, 8, size=(self.num_solutions, 8))
 
         self.vector_to_matrix()
 
-        self.pop_created = True  # Indicates population_board is ready.
+        self.pop_created = True
         self.num_attacks_Label.text = "Initial population_board Created."
 
-    # Convert 1D vector representation to 2D matrix form
     def vector_to_matrix(self):
-        """
-        Converts the 1D vector representation of queen positions into a 2D matrix form for the board.
-        Each solution (vector) is transformed into an 8x8 grid representation.
-        """
         self.population_board = numpy.zeros(shape=(self.num_solutions, 8, 8))
 
         for solution_idx, current_solution in enumerate(self.population_1D_vector):
@@ -98,25 +73,16 @@ class Queens8App(kivy.app.App):
             for row_idx, col_idx in enumerate(current_solution):
                 self.population_board[solution_idx, row_idx, col_idx] = 1
 
-    # Reset the chessboard UI
     def reset_board_text(self):
-        """
-        Resets the chessboard UI, clearing all labels and resetting cell colors.
-        """
         for row_idx in range(self.all_widgets.shape[0]):
             for col_idx in range(self.all_widgets.shape[1]):
                 self.all_widgets[row_idx, col_idx].text = "[color=262626]" + str(row_idx) + ", " + str(col_idx) + "[/color]"
                 with self.all_widgets[row_idx, col_idx].canvas.before:
-                    kivy.graphics.Color(0, 0, 0, 1)  # Black background
+                    kivy.graphics.Color(0, 0, 0, 1)
                     self.rect = kivy.graphics.Rectangle(size=self.all_widgets[row_idx, col_idx].size, pos=self.all_widgets[row_idx, col_idx].pos)
 
-    # Update the board to display the best solution
     def update_board_UI(self, *args):
-        """
-        Updates the chessboard UI with the best solution from the current population_board.
-        Displays queen positions and updates fitness information.
-        """
-        if self.pop_created == False:
+        if not self.pop_created:
             return
 
         self.reset_board_text()
@@ -134,42 +100,28 @@ class Queens8App(kivy.app.App):
                 if best_solution[row_idx, col_idx] == 1:
                     self.all_widgets[row_idx, col_idx].text = "[color=ffcc00]Queen[/color]"
                     with self.all_widgets[row_idx, col_idx].canvas.before:
-                        kivy.graphics.Color(0, 1, 0, 1)  # Green for queens
+                        kivy.graphics.Color(0, 1, 0, 1)
                         self.rect = kivy.graphics.Rectangle(size=self.all_widgets[row_idx, col_idx].size, pos=self.all_widgets[row_idx, col_idx].pos)
 
-    # Calculate the fitness of each solution
     def fitness(self, population_board):
-        """
-        Calculates the fitness of each solution in the population_board based on the number of attacks.
-        Higher fitness is assigned to solutions with fewer attacks.
-        """
         total_num_attacks_column = self.attacks_column(self.population_board)
-        print("Number of Attacks (Column) : ", total_num_attacks_column)
-
         total_num_attacks_diagonal = self.attacks_diagonal(self.population_board)
-        print("Number of Attacks (Diagonal) : ", total_num_attacks_diagonal)
+        total_num_attacks_horizontal = self.attacks_horizontal(self.population_board)
 
-        total_num_attacks = total_num_attacks_column + total_num_attacks_diagonal
+        total_num_attacks = total_num_attacks_column + total_num_attacks_diagonal + total_num_attacks_horizontal
 
         population_fitness = numpy.copy(total_num_attacks)
 
-        print("Number of Attacks (Column + Diagonal) : ", total_num_attacks)
-
         for solution_idx in range(population_board.shape[0]):
             if population_fitness[solution_idx] == 0:
-                population_fitness[solution_idx] = float("inf")  # Perfect solution
+                population_fitness[solution_idx] = float("inf")
             else:
-                population_fitness[solution_idx] = 1.0 / population_fitness[solution_idx]  # Lower attacks = higher fitness
-
-        print("population_board Fitness (Column + Diagonal) : ", population_fitness)
+                population_fitness[solution_idx] = 1.0 / population_fitness[solution_idx]
 
         return population_fitness, total_num_attacks
 
-    # Calculate diagonal attacks for each solution
     def attacks_diagonal(self, population_board):
-        """
-        Counts the number of diagonal attacks (both major and minor diagonals) for each solution.
-        """
+
         total_num_attacks = numpy.zeros(population_board.shape[0])
 
         for solution_idx in range(population_board.shape[0]):
@@ -196,20 +148,14 @@ class Queens8App(kivy.app.App):
 
         return total_num_attacks
 
-    # Helper for diagonal attacks
     def diagonal_attacks(self, mat):
-        """
-        Returns the number of queen attacks along the diagonal of a sub-matrix.
-        """
+
         if mat.shape[0] < 2 or mat.shape[1] < 2:
             return 0
         return mat.diagonal().sum() - 1
-
-    # Calculate column attacks for each solution
+    
     def attacks_column(self, population_board):
-        """
-        Counts the number of column-wise attacks for each solution in the population_board.
-        """
+
         total_num_attacks = numpy.zeros(population_board.shape[0])
 
         for solution_idx in range(population_board.shape[0]):
@@ -222,7 +168,20 @@ class Queens8App(kivy.app.App):
 
         return total_num_attacks
 
-    # Build the GUI
+    def attacks_horizontal(self, population_board):
+        total_num_attacks = numpy.zeros(population_board.shape[0])
+
+        for solution_idx in range(population_board.shape[0]):
+            ga_solution = population_board[solution_idx, :]
+
+            for queen_x_pos in range(8):
+                row_sum = numpy.sum(ga_solution[queen_x_pos, :])
+                if row_sum > 1:
+                    total_num_attacks[solution_idx] += row_sum - 1
+
+        return total_num_attacks
+
+
     def build(self):
         """
         Builds the graphical user interface (GUI) for the application, 
@@ -251,8 +210,8 @@ class Queens8App(kivy.app.App):
         start_ga_button.bind(on_press=self.start_ga)
 
         # Inputs for GA parameters
-        self.num_solutions_TextInput = kivy.uix.textinput.TextInput(text="8", font_size=20, size_hint_x=1)
-        self.num_generations_TextInput = kivy.uix.textinput.TextInput(text="10000", font_size=20, size_hint_x=1)
+        self.num_solutions_TextInput = kivy.uix.textinput.TextInput(text="150", font_size=20, size_hint_x=1)
+        self.num_generations_TextInput = kivy.uix.textinput.TextInput(text="500", font_size=20, size_hint_x=1)
         self.num_mutations_TextInput = kivy.uix.textinput.TextInput(text="5", font_size=20, size_hint_x=1)
 
         # Display for fitness/attack stats

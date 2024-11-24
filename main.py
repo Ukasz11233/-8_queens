805d2e7d0c297de3a2c346f007556bd42983c26c
Kuba Banach <jakbanach@student.agh.edu.pl>
2024-11-24 15:37:47 +0100

    added visualization in real time

--- main.py
+++ main.py
@@ -7,58 +7,83 @@ import kivy.uix.label
 import kivy.graphics
 import numpy
 import genAlg
+from kivy.clock import Clock
 
+N = 8
 class Queens8App(kivy.app.App):
     pop_created = False
 
     def start_ga(self, *args):
         self.initialize_population()
-        best_outputs = []
-        best_outputs_fitness = []
+        self.best_outputs = []
+        self.best_outputs_fitness = []
         if not self.pop_created:
             return
 
-        num_generations = numpy.uint16(self.num_generations_TextInput.text)
-        num_parents = numpy.uint8(self.num_solutions / 2)
+        self.num_generations = numpy.uint16(self.num_generations_TextInput.text)
+        self.num_parents = numpy.uint8(self.num_solutions / 2)
+        self.generation = 0
+        self.best_score = 1000
 
-        for generation in range(num_generations):
-            print("\n##  Generation = ", generation, "  ##\n")
+        Clock.schedule_interval(self.run_generation, 0.1)
 
-            population_fitness, total_num_attacks = self.fitness(self.population_board)
+    def run_generation(self, dt):
+        if self.generation>=self.num_generations or self.best_score == 0:
+            Clock.unschedule(self.run_generation)
+            return
 
-            max_fitness = numpy.max(population_fitness)
-            max_fitness_idx = numpy.where(population_fitness == max_fitness)[0][0]
+        # print("\n##  Generation = ", generation, "  ##\n")
 
-            best_outputs_fitness.append(max_fitness)
-            best_outputs.append(self.population_1D_vector[max_fitness_idx, :])
+        population_fitness, total_num_attacks = self.fitness(self.population_board)
 
-            if max_fitness == float("inf"):
-                print("Best solution found")
-                self.num_attacks_Label.text = "Best Solution Found"
-                print("\n1D population_board : \n", self.population_1D_vector)
-                print("\n**  Best solution IDX = ", max_fitness_idx, "  **\n")
+        max_fitness = numpy.max(population_fitness)
+        max_fitness_idx = numpy.where(population_fitness == max_fitness)[0][0]
 
-                numpy.save("best_outputs_fitness.npy", best_outputs_fitness)
-                numpy.save("best_outputs.npy", best_outputs)
-                print("\n**  Data Saved Successfully  **\n")
+        self.best_outputs_fitness.append(max_fitness)
+        self.best_outputs.append(self.population_1D_vector[max_fitness_idx, :])
 
-                break
 
-            parents = genAlg.select_parents(self.population_1D_vector, population_fitness, num_parents)
-            offspring_crossover = genAlg.crossover(parents, offspring_size=(self.num_solutions - parents.shape[0], 8))
-            offspring_mutation = genAlg.mutation(offspring_crossover, num_mutations=numpy.uint8(self.num_mutations_TextInput.text))
+        if total_num_attacks[max_fitness_idx] < self.best_score:
+            self.best_score = total_num_attacks[max_fitness_idx]
+            self.update_board_UI()
+            
 
-            self.population_1D_vector[0:parents.shape[0], :] = parents
-            self.population_1D_vector[parents.shape[0]:, :] = offspring_mutation
 
-            self.vector_to_matrix()
+        if self.generation % 10 == 0:
+            print("Generation = ", self.generation, "  Max Fitness = ", max_fitness, "  # Attacks = ",
+                    total_num_attacks[max_fitness_idx])
+        #     self.update_board_UI()
+        #     # wait for 1 second to display the best solution
+        #     kivy.clock.Clock.schedule_once(self.update_board_UI, 1)
+
+        if max_fitness == float("inf"):
+            print("Best solution found")
+            self.num_attacks_Label.text = "Best Solution Found"
+            print("\n1D population_board : \n", self.population_1D_vector)
+            print("\n**  Best solution IDX = ", max_fitness_idx, "  **\n")
+
+            numpy.save("best_outputs_fitness.npy", self.best_outputs_fitness)
+            numpy.save("best_outputs.npy", self.best_outputs)
+            print("\n**  Data Saved Successfully  **\n")
+
+            return
+
+        parents = genAlg.select_parents(self.population_1D_vector, population_fitness, self.num_parents)
+        offspring_crossover = genAlg.crossover(parents, offspring_size=(self.num_solutions - parents.shape[0], N))
+        offspring_mutation = genAlg.mutation(offspring_crossover, num_mutations=numpy.uint8(self.num_mutations_TextInput.text))
+
+        self.population_1D_vector[0:parents.shape[0], :] = parents
+        self.population_1D_vector[parents.shape[0]:, :] = offspring_mutation
+        self.generation += 1
+
+        self.vector_to_matrix()
 
     def initialize_population(self, *args):
         self.num_solutions = numpy.uint8(self.num_solutions_TextInput.text)
 
         self.reset_board_text()
 
-        self.population_1D_vector = numpy.random.randint(0, 8, size=(self.num_solutions, 8))
+        self.population_1D_vector = numpy.random.randint(0, N, size=(self.num_solutions, N))
 
         self.vector_to_matrix()
 
@@ -66,7 +91,7 @@ class Queens8App(kivy.app.App):
         self.num_attacks_Label.text = "Initial population_board Created."
 
     def vector_to_matrix(self):
-        self.population_board = numpy.zeros(shape=(self.num_solutions, 8, 8))
+        self.population_board = numpy.zeros(shape=(self.num_solutions, N, N))
 
         for solution_idx, current_solution in enumerate(self.population_1D_vector):
             current_solution = numpy.uint8(current_solution)
@@ -97,15 +122,15 @@ class Queens8App(kivy.app.App):
         for solution_idx in range(population_board.shape[0]):
             ga_solution = population_board[solution_idx, :]
 
-            temp = numpy.zeros(shape=(10, 10))
-            temp[1:9, 1:9] = ga_solution
+            temp = numpy.zeros(shape=(N+2, N+2))
+            temp[1:N+1, 1:N+1] = ga_solution
 
             row_indices, col_indices = numpy.where(ga_solution == 1)
             row_indices += 1
             col_indices += 1
 
             total = 0
-            for element_idx in range(8):
+            for element_idx in range(N):
                 x = row_indices[element_idx]
                 y = col_indices[element_idx]
 
@@ -131,7 +156,7 @@ class Queens8App(kivy.app.App):
         for solution_idx in range(population_board.shape[0]):
             ga_solution = population_board[solution_idx, :]
 
-            for queen_y_pos in range(8):
+            for queen_y_pos in range(N):
                 col_sum = numpy.sum(ga_solution[:, queen_y_pos])
                 if col_sum > 1:
                     total_num_attacks[solution_idx] += col_sum - 1
@@ -144,7 +169,7 @@ class Queens8App(kivy.app.App):
         for solution_idx in range(population_board.shape[0]):
             ga_solution = population_board[solution_idx, :]
 
-            for queen_x_pos in range(8):
+            for queen_x_pos in range(N):
                 row_sum = numpy.sum(ga_solution[queen_x_pos, :])
                 if row_sum > 1:
                     total_num_attacks[solution_idx] += row_sum - 1
@@ -165,27 +190,32 @@ class Queens8App(kivy.app.App):
     def update_board_UI(self, *args):
         if not self.pop_created:
             return
+        
+        def update_ui(*_):
+            self.reset_board_text()
+            print("Updating UI for generation = ", self.generation, "# Attacks = ", self.best_score)
 
-        self.reset_board_text()
+            population_fitness, total_num_attacks = self.fitness(self.population_board)
 
-        population_fitness, total_num_attacks = self.fitness(self.population_board)
+            max_fitness = numpy.max(population_fitness)
+            max_fitness_idx = numpy.where(population_fitness == max_fitness)[0][0]
+            best_solution = self.population_board[max_fitness_idx, :]
 
-        max_fitness = numpy.max(population_fitness)
-        max_fitness_idx = numpy.where(population_fitness == max_fitness)[0][0]
-        best_solution = self.population_board[max_fitness_idx, :]
+            self.num_attacks_Label.text = f"Max Fitness = {max_fitness:.4f}\n# Attacks = {total_num_attacks[max_fitness_idx]}"
 
-        self.num_attacks_Label.text = "Max Fitness = " + str(numpy.round(max_fitness, 4)) + "\n# Attacks = " + str(
-            total_num_attacks[max_fitness_idx])
+            for row_idx in range(N):
+                for col_idx in range(N):
+                    if best_solution[row_idx, col_idx] == 1:
+                        self.all_widgets[row_idx, col_idx].text = "w"
+                        self.all_widgets[row_idx, col_idx].color = (1, 209/255, 0, 1)
+                        with self.all_widgets[row_idx, col_idx].canvas.before:
+                            kivy.graphics.Color(0, 1, 0, 1)
+                            self.rect = kivy.graphics.Rectangle(
+                                size=self.all_widgets[row_idx, col_idx].size,
+                                pos=self.all_widgets[row_idx, col_idx].pos
+                            )
 
-        for row_idx in range(8):
-            for col_idx in range(8):
-                if best_solution[row_idx, col_idx] == 1:
-                    self.all_widgets[row_idx, col_idx].text = "w"
-                    self.all_widgets[row_idx, col_idx].color = (1, 209/255, 0, 1)
-                    with self.all_widgets[row_idx, col_idx].canvas.before:
-                        kivy.graphics.Color(0, 1, 0, 1)
-                        self.rect = kivy.graphics.Rectangle(size=self.all_widgets[row_idx, col_idx].size,
-                                                            pos=self.all_widgets[row_idx, col_idx].pos)
+        Clock.schedule_once(update_ui)
 
     def build(self):
         """
@@ -195,14 +225,14 @@ class Queens8App(kivy.app.App):
         boxLayout = kivy.uix.boxlayout.BoxLayout(orientation="vertical" )
 
         # Grid layout for the chessboard
-        gridLayout = kivy.uix.gridlayout.GridLayout(rows=8, cols=8, size_hint_y=80)
-        boxLayout_buttons = kivy.uix.boxlayout.BoxLayout(orientation="horizontal", size_hint_y=8)
+        gridLayout = kivy.uix.gridlayout.GridLayout(rows=N, cols=N, size_hint_y=80)
+        boxLayout_buttons = kivy.uix.boxlayout.BoxLayout(orientation="horizontal", size_hint_y=N)
 
         boxLayout.add_widget(gridLayout)
         boxLayout.add_widget(boxLayout_buttons)
 
         # Initialize the chessboard widget array
-        self.all_widgets = numpy.zeros(shape=(8, 8), dtype="O")
+        self.all_widgets = numpy.zeros(shape=(N, N), dtype="O")
 
         # Add buttons to the chessboard with alternating colors
         for row_idx in range(self.all_widgets.shape[0]):

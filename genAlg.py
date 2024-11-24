7ec6d52d0a2aa36a7083b8269956eb58018d7abe
Ukasz11233 <chmiel11233@gmail.com>
2024-11-24 20:15:05 +0100

    refactored population representation to use coordinates

--- genAlg.py
+++ genAlg.py
@@ -1,29 +1,30 @@
 import numpy
 
 def select_parents(pop, fitness, num_parents):
-    parents = numpy.empty((num_parents, pop.shape[1]))
-    for parent_num in range(num_parents):
-        max_fitness_idx = numpy.where(fitness == numpy.max(fitness))
+    parents = []
+    fitness_copy = fitness.copy()
+    for _ in range(num_parents):
+        max_fitness_idx = numpy.where(fitness_copy == numpy.max(fitness_copy))
         max_fitness_idx = max_fitness_idx[0][0]
-        parents[parent_num, :] = pop[max_fitness_idx, :]
-        fitness[max_fitness_idx] = -float("Inf")
+        parents.append(pop[max_fitness_idx])
+        fitness_copy[max_fitness_idx] = -float("Inf")
     return parents
 
 def crossover(parents, offspring_size):
-    offspring = numpy.empty(offspring_size)
-    crossover_point = numpy.uint8(offspring_size[1] / 2)
+    offspring = []
+    crossover_point = offspring_size[1] // 2
 
     for k in range(offspring_size[0]):
-        parent1_idx = k % parents.shape[0]
-        parent2_idx = (k + 1) % parents.shape[0]
-        offspring[k, 0:crossover_point] = parents[parent1_idx, 0:crossover_point]
-        offspring[k, crossover_point:] = parents[parent2_idx, crossover_point:]
+        parent1_idx = k % len(parents)
+        parent2_idx = (k + 1) % len(parents)
+        offspring_1 = parents[parent1_idx][:crossover_point] + parents[parent2_idx][crossover_point:]
+        offspring.append(offspring_1)
     return offspring
 
-def mutation(offspring_crossover, num_mutations):
-    for idx in range(offspring_crossover.shape[0]):
-        for mutation_num in range(num_mutations):
-            gene_idx = numpy.random.randint(low=0, high=8)  # Flip a queen's position
-            new_value = numpy.random.randint(0, 8)
-            offspring_crossover[idx, gene_idx] = new_value
-    return offspring_crossover
+def mutation(offspring, num_mutations):
+    for idx in range(len(offspring)):
+        for _ in range(num_mutations):
+            gene_idx = numpy.random.randint(low=0, high=len(offspring[idx]))
+            new_value = (numpy.random.randint(0, 8), numpy.random.randint(0, 8))
+            offspring[idx][gene_idx] = new_value
+    return offspring
